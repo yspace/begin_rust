993fc34128282f6dd176f75a34f8fb4695314632
yiqing <yiqing_95@qq.com>
2020-07-03 10:17:50 +0800

    使用绝对路径 相对路径引用方法

--- extc/restaurant/src/lib.rs
+++ extc/restaurant/src/lib.rs
@@ -1,32 +1,13 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist(){
-
-        }
-        fn seat_at_tabble(){
-
-        }
-    }
-
-    mod serving{
-        fn take_order(){
-
-        }
-        fn server_order(){
-
-        }
-        fn take_payment(){
-
-        }
+    pub mod hosting {
+      pub  fn add_to_waitlist() {}
     }
 }
 
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
 
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
