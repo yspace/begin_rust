cf396d3744c16a4356d8d44650e297ef6ebd5caf
yiqing <yiqing_95@qq.com>
2020-07-03 10:13:44 +0800

    测试外部crate

---
+++ extc/restaurant/src/lib.rs
@@ -0,0 +1,32 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist(){
+
+        }
+        fn seat_at_tabble(){
+
+        }
+    }
+
+    mod serving{
+        fn take_order(){
+
+        }
+        fn server_order(){
+
+        }
+        fn take_payment(){
+
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
