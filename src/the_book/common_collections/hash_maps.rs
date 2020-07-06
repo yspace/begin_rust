d892c1cdd6a7934d44499134e06d95795cc2e3bb
yiqing <yiqing_95@qq.com>
2020-07-06 17:00:35 +0800

    hash map 常用操作 增改

--- src/the_book/common_collections/hash_maps.rs
+++ src/the_book/common_collections/hash_maps.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 
 pub fn action_hash_map(){
     create() ;
+    access_values();
+    iter() ;
+    update() ;
 }
 
 fn create(){
@@ -34,4 +37,64 @@ fn ownership(){
     // field_name and field_value are invalid at this point, try using them and
     // see what compiler error you get!
 //    println!("{} {}", field_name, field_value);
+}
+
+fn access_values(){
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    match score {
+        Some(v)=> {
+          println!("{}:{}", team_name,  v);
+        },
+        None =>{
+            println!("has no value for {}", team_name) ;
+        },
+    }
+}
+
+fn iter(){
+    let mut scores = HashMap::new() ;
+
+    scores.insert(String::from("Blue"), 10) ;
+    scores.insert(String::from("Yellow"), 50) ;
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value) ;
+    }
+}
+
+fn update(){
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25); // 替换旧值
+
+    println!("{:?}", scores);
+
+    // ## Only Inserting a Value If the Key Has No Value
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("UC_ENTRY: {:?}", scores);
+
+    // ## Updating a  Value based on the old Value
+    let text = "hello world wonderful world" ;
+    let mut map = HashMap::new() ;
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1 ;
+    }
+
+    println!("{:?}", map) ;
 }
