2d7e338b2430fdbaa41d7df9bec09f35de2cd4cf
yiqing <yiqing_95@qq.com>
2020-07-19 09:52:39 +0800

    lifetimes 也是一种generic

--- src/the_book/generic_types_traits_lifetimes/lifetimes.rs
+++ src/the_book/generic_types_traits_lifetimes/lifetimes.rs
@@ -113,6 +113,9 @@ fn longest_with_an_announcement<'a, T>(
     where
         T: Display,
 {
+    // Because lifetimes are a type of generic, the declarations of the lifetime parameter 'a
+    // and the generic type parameter T go in the same list inside the angle brackets after the function name.
+
     println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
