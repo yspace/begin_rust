80ee0cc975e9734b6f353a49645d3d1c415ec77b
yiqing <yiqing_95@qq.com>
2020-07-20 23:29:28 +0800

    控制测试

--- src/my_lib.rs
+++ src/my_lib.rs
@@ -101,6 +101,16 @@ impl Guess {
     }
 }
 
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
+pub fn add_two2(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -182,5 +192,39 @@ mod tests {
         } else {
             Err(String::from("two plus two does not equal four"))
         }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
     }
 }
