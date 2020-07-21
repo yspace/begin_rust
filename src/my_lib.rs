5cd4c447fed3f624b4bf6ad3f3cfc57efb1fab58
yiqing <yiqing_95@qq.com>
2020-07-21 12:55:38 +0800

    私有方法也可以测试呢

--- src/my_lib.rs
+++ src/my_lib.rs
@@ -227,4 +227,17 @@ mod tests {
     fn expensive_test() {
         // code that takes an hour to run
     }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
+
+pub fn add_two3(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
