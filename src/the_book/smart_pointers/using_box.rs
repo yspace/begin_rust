0f4cb0589257efe926fc27fbbc4b4450e36da260
yiqing <yiqing_95@qq.com>
2020-08-15 22:08:07 +0800

    Definition of List that uses Box<T> in order to have a known size

--- src/the_book/smart_pointers/using_box.rs
+++ src/the_book/smart_pointers/using_box.rs
@@ -17,7 +17,7 @@ fn using_box(){
 //  Enabling Recursive Types with Boxes
 
 enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil ,
 }
 
@@ -25,11 +25,13 @@ fn using_list_type(){
 //    use List::{Cons, Nil} ;
 use self::List::{Cons, Nil} ;
 
-    let list = Cons(1,
-        Cons(2,
-            Cons(3, Nil)
-        )
-    );
+//    let list = Cons(1,
+//        Cons(2,
+//            Cons(3, Nil)
+//        )
+//    );
 
-    println!("{:?}", list) ;
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+
 }
