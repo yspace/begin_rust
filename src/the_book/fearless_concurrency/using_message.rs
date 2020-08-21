9ff58893460450dbcdece7757f628bee935bc99f
yiqing <yiqing_95@qq.com>
2020-08-21 13:31:02 +0800

    Creating Multiple Producers by Cloning the Transmitter

--- src/the_book/fearless_concurrency/using_message.rs
+++ src/the_book/fearless_concurrency/using_message.rs
@@ -16,6 +16,8 @@ pub fn action_using_message_passing(){
 
     //
     sending_multiple_values();
+    //Creating Multiple Producers by Cloning the Transmitter
+    creating_multiple_producers() ;
 }
 
 fn use_val_after_sent(){
@@ -53,5 +55,44 @@ fn sending_multiple_values(){
 
     for received in rx {
         println!("Got: {}", received) ;
+    }
+}
+
+// Sending multiple messages from multiple producers
+fn creating_multiple_producers(){
+    // 故事： 类似父母有多个子女在外打工 可以不定期的发送消息给父母
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
     }
 }
