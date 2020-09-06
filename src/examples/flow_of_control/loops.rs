0b5cfbb2bc59ae3c3a5417255f1d635897d86a38
yiqing <yiqing_95@qq.com>
2020-09-07 07:08:17 +0800

    loop的返回值

--- src/examples/flow_of_control/loops.rs
+++ src/examples/flow_of_control/loops.rs
@@ -25,4 +25,49 @@ pub fn act_main(){
         }
 
     }
+
+    //
+    nesting_and_labels::act_main() ;
+    returning_from_loops::act_main() ;
+}
+
+pub mod nesting_and_labels{
+    #![allow(unreachable_code)]
+
+    pub fn act_main(){
+        'outer: loop {
+            println!("Entered the outer loop") ;
+
+            'inner: loop{
+                println!("Entered the inner loop");
+
+                // 这个只会终结内部循环
+                // break ;
+
+                // 这个就会跳出外部循环
+                break 'outer ;
+            }
+
+            println!("This point will never be reached ");
+        }
+
+        println!("Exited the outer loop") ;
+    }
+}
+
+pub mod returning_from_loops{
+    pub fn act_main(){
+        let mut counter = 0 ;
+
+        let result = loop {
+            counter += 1 ;
+
+            if counter == 10{
+                // 会作为loop的值返回出去的
+                break counter * 2 ;
+            }
+        };
+
+        assert_eq!(result , 20) ;
+    }
 }
