3433ea76f21db49a9b0e0487e06c48a66d38cf85
yiqing <yiqing_95@qq.com>
2020-09-09 14:41:27 +0800

    元组枚举的解构

--- src/examples/flow_of_control/matches.rs
+++ src/examples/flow_of_control/matches.rs
@@ -25,4 +25,67 @@ pub fn act_main(){
 
     };
     println!("{} -> {}", boolean, binary) ;
+
+    destructuring::tuples::act_main() ;
+    destructuring::enums::act_main() ;
+}
+
+mod destructuring{
+    pub mod tuples{
+        pub fn act_main(){
+            let pair = (0, -2);
+
+            println!("Tell me about {:?}", pair);
+
+            match pair {
+                (0,y) => println!("First is `0` and y is `{:?}`", y),
+                (x,0) => println!("`x` is `{:?}` and last is `0`", x),
+                _ => println!("It does't matter what they are"),
+                // `_`意味着不绑定值到任何变量
+            }
+        }
+    }
+
+    pub mod enums{
+        // `allow` required to silence warnings because only
+// one variant is used.
+        #[allow(dead_code)]
+        enum Color {
+            // These 3 are specified solely by their name.
+            Red,
+            Blue,
+            Green,
+            // These likewise tie `u32` tuples to different names: color models.
+            RGB(u32, u32, u32),
+            HSV(u32, u32, u32),
+            HSL(u32, u32, u32),
+            CMY(u32, u32, u32),
+            CMYK(u32, u32, u32, u32),
+        }
+
+        pub fn act_main(){
+            let color = Color::RGB(122, 17, 40);
+            // TODO ^ Try different variants for `color`
+
+            println!("What color is it?");
+            // An `enum` can be destructured using a `match`.
+            match color {
+                Color::Red   => println!("The color is Red!"),
+                Color::Blue  => println!("The color is Blue!"),
+                Color::Green => println!("The color is Green!"),
+                Color::RGB(r, g, b) =>
+                    println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+                Color::HSV(h, s, v) =>
+                    println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+                Color::HSL(h, s, l) =>
+                    println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+                Color::CMY(c, m, y) =>
+                    println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+                Color::CMYK(c, m, y, k) =>
+                    println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+                             c, m, y, k),
+                // Don't need another arm because all variants have been examined
+            }
+        }
+    }
 }
